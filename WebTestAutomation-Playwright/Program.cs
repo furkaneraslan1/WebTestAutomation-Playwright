568ff1533715f1be7fcd4b2629e586766aa2005b
Furkan Eraslan <eraslanfurkan1@gmail.com>
2025-03-10 11:41:02 +0100

    Initialize Playwright

---
+++ WebTestAutomation-Playwright/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Playwright;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://example.com");
+    }
+}
